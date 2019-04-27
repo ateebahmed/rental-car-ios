5f9ec555600176231ae45db16165af2982db893a
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-04-27 20:38:55 +0500

    Initial Commit

---
+++ Rent24/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Rent24
+//
+//  Created by Ateeb Ahmed on 27/04/2019.
+//  Copyright © 2019 Ateeb Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Rent24/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Rent24
+//
+//  Created by Ateeb Ahmed on 27/04/2019.
+//  Copyright © 2019 Ateeb Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
