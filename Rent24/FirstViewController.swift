c7494c741ffc3cbf4a8e35127ba978a818f4667e
Ateeb Ahmed <ateeb_ahmed33@hotmail.com>
2019-04-28 23:54:11 +0500

    initial project structure and UI

--- Rent24/FirstViewController.swift
+++ Rent24/FirstViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class JobViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ Rent24/Invoice/InvoiceViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InvoiceViewController.swift
+//  Rent24
+//
+//  Created by Ateeb Ahmed on 28/04/2019.
+//  Copyright © 2019 Ateeb Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class InvoiceViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
